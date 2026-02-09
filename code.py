65ff96679a915cd070fb0086b616a37033951a27
Analyzer <analyzer@test.com>
2026-02-09 10:33:52 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
-n = int(input())
-scores = list(map(int, input().split()))
-min_score = min(scores)
-third_student_score = scores[2]
-modified_score = (min_score ^ third_student_score) + 5
-print(modified_score)
+k = int(input())
+nums = list(map(int, input().split()))
+
+s = {0}
+for num in nums:
+    for current in list(s):
+        s.add(current + num)
+
+print(len(s))
