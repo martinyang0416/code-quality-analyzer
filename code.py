b2f6c90c65ad97748a49de9aef7f40d7ca0610a3
Analyzer <analyzer@test.com>
2026-02-09 01:25:59 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def is_lucky(s):
+    return all(c in {'0', '4', '7'} for c in s)
 
-import builtins
-import re
+def solve():
+    import sys
+    input = sys.stdin.read().split()
+    t = int(input[0])
+    cases = input[1:t+1]
+
+    # Precompute possible sums for 6 digits of 0,4,7
+    max_sum = 6*7
+    possible = [False] * (max_sum + 1)
+    # Generate all possible sums with exactly 6 digits
+    from itertools import product
+    for digits in product([0,4,7], repeat=6):
+        s = sum(digits)
+        possible[s] = True
+
+    for n_str in cases:
