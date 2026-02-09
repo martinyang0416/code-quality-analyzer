346f8b8150d277a69cd8d0cd0e3ee3418d44dedb
Analyzer <analyzer@test.com>
2026-02-09 01:26:57 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
+import heapq
 import sys
-from collections import deque
+from collections import defaultdict
 
-class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n + 1))
-        self.size = [1] * (n + 1)
-        self.diameter = [0] * (n + 1)
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        if x_root == y_root:
-            return
-        if self.size[x
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    n = int(input[ptr]); ptr +=1
+    m = int(input[ptr]); ptr +=1
+    L = int(input[ptr]); ptr +=1
+    s = int(input[ptr]); ptr +=1
+    t = int(input[ptr]); ptr +=1
+
+    edges = []
+    adj_fixed = [[] for _ in range(n)]
+    adj_S = [[] for _ in range(n)]
+    original_edges = []
+    for _ in range(m):
+        u = int(input[ptr]); ptr +=1
+        v = int(input[ptr]); ptr +=1
+  
