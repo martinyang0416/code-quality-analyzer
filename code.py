5e4b8afca24bd2d4e427f0d2f363d616bb844d08
Analyzer <analyzer@test.com>
2026-02-09 10:28:16 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,12 +1,11 @@
-n = int(input())
-a = list(map(int, input().split()))
-prefix = [0]
-for num in a:
-    prefix.append(prefix[-1] + num)
-result = []
-for k in range(1, n + 1):
-    sum_first = prefix[k]
-    sum_last = prefix[n] - prefix[n - k]
-    if sum_first == sum_last:
-        result.append(str(k))
-print(' '.join(result))
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    a = list(map(int, input().split()))
+    seen = set()
+    p = []
+    for num in a:
+        if num not in seen:
+            p.append(num)
+            seen.add(num)
+    print(' '.join(map(str, p)))
