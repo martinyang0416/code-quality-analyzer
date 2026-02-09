751a38c2670bf5a493b0d5d94b189a944328b782
Analyzer <analyzer@test.com>
2026-02-09 10:18:17 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
-b = int(input())
-m = input().strip()
+import sys
 
-if m == '0':
-    print(0)
-else:
-    res = 0
-    for c in m:
-        d = int(c)
-        res = res * b + d
-    print(res)
+def main():
+    n = int(sys.stdin.readline())
+    c = list(map(int, sys.stdin.readline().split()))
+    s = list(map(int, sys.stdin.readline().split()))
+    
+    max_sum = 0
+    
+    for i in range(n):
+        a = c[i]
+        sa = s[i]
+        for j in range(i + 1, n):
+            b = c[j]
+            sb = s[j]
+            # Check if neither a is subset of b nor b is subset of a
+            intersect = a & b
+            if (intersect == a) or (intersect == b):
+                continu
