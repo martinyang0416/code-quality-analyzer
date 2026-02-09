8d2c74d2ec3eac150358388e54c06c142c751c1d
Analyzer <analyzer@test.com>
2026-02-09 10:25:40 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,22 +1,22 @@
-n, *rest = map(int, open(0).read().split())
-a = rest[:n]
+n = int(input())
+b = list(map(int, input().split()))
+sum_all = sum(b)
+active = [True] * (n + 1)  # active[i] indicates if token i is still active
+total_gain = 0
 
-from collections import defaultdict
+for x in range(n, 0, -1):
+    current_sum = 0
+    m = x
+    while m <= n:
+        if active[m]:
+            current_sum += -b[m-1]  # b is 0-based
+        m += x
+    if current_sum > 0:
+        total_gain += current_sum
+        m = x
+        while m <= n:
+            if active[m]:
+                active[m] = False
+            m += x
 
-freq = defaultdict(int)
-for num in a:
-    freq[num] += 1
-
-unique = list(freq.keys())
-len_unique = len(unique)
-
-if len_unique >= 3:
-    print(3)
-elif len_unique == 2:
-    a1, a2 = unique[0], unique[1]
-    if freq[a1] >= 2 and freq[a2] >= 2:
-        print(4)
-    else:
-        print(-1)
-else:
-    print(-1)
+pr
