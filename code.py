92d01f6ae0fb672a63104917cbe7d5f0676dba85
Analyzer <analyzer@test.com>
2026-02-09 10:38:36 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,22 +1,21 @@
-s = input().strip()
+import sys
 
-args = []
-current_arg = []
-state = 'outside'
-
-for char in s:
-    if state == 'outside':
-        if char == ' ':
-            if current_arg:
-                args.append(''.join(current_arg))
-                current_arg = []
-        elif char == '[':
-            if current_arg:
-                args.append(''.join(current_arg))
-                current_arg = []
-            state = 'inside'
+def decode_caesar(line):
+    result = []
+    n = 0
+    for c in line:
+        if c.isalpha():
+            encrypted_pos = ord(c) - ord('a')
+            shift = (n + 1) % 26
+            original_pos = (encrypted_pos - shift) % 26
+            result.append(chr(original_pos + ord('a')))
         else:
-            current_arg.append(char)
-    else:
-        if char == ']':
-     
+            result.append(c)
+        n += 1
+    return ''.join(result)
+
+def main():
+    for line in sys.stdin:
+        line = line.rstrip('\n')
+        decoded_line = decode_caesar(line)
+  
