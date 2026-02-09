d5baf2dba366d6898d2c677c13bd395b7ac14628
Analyzer <analyzer@test.com>
2026-02-09 09:53:29 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,9 +1,19 @@
-m = int(input())
-A = list(map(int, input().split()))
 n = int(input())
-B = list(map(int, input().split()))
-C = A + B
-max_val = max(C)
-min_val = min(C)
-result = max_val - min_val
-print("{0:.9f}".format(result))
+digits = input().split()
+
+result = []
+for i in range(n):
+    d = digits[i]
+    if (i + 1) % 2 == 1:  # Odd index operation
+        result.append(d)
+    else:  # Even index operation
+        if not result:
+            result.append(d)
+        else:
+            last = result[-1]
+            if d == last:
+                result.append(d)
+            else:
+                # Remove all trailing same as last
+                while result and result[-1] == last:
+                    resu
