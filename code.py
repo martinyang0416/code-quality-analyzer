d34ae9e094a960273629a85e6084fd28389ac970
Analyzer <analyzer@test.com>
2026-02-09 10:10:41 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,3 +1,18 @@
-n, a, b = map(int, input().split())
-new_index = (a - 1 + b) % n
-print(new_index + 1)
+n = int(input())
+x, y = 0, 0
+directions = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    new_x_plus = x + a
+    new_y_plus = y + b
+    d_plus = abs(new_x_plus) + abs(new_y_plus)
+    new_x_minus = x - a
+    new_y_minus = y - b
+    d_minus = abs(new_x_minus) + abs(new_y_minus)
+    if d_plus < d_minus:
+        directions.append(1)
+        x, y = new_x_plus, new_y_plus
+    else:
+        directions.append(-1)
+        x, y = new_x_minus, new_y_minus
+print(' '.join(map(str, directions))
