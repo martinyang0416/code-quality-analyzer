1cc464ffafdba1f05ba61d5ab835005543717fc3
Analyzer <analyzer@test.com>
2026-02-09 10:31:59 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,23 +1,15 @@
-def solve():
-    import sys
-    from collections import defaultdict
+def count_odd_pairs(s):
+    even = 0
+    odd = 0
+    for char in s:
+        pos = ord(char) - ord('a') + 1
+        if pos % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return even * odd
 
-    t = sys.stdin.readline().strip()
-    k = int(sys.stdin.readline())
-
-    n = len(t)
-    cnt = defaultdict(int)
-    for c in t:
-        cnt[c] += 1
-
-    divisors = set()
-    for d in range(1, int(k**0.5) + 1):
-        if k % d == 0:
-            if d <= n:
-                divisors.add(d)
-            if (k // d) <= n:
-                divisors.add(k // d)
-    divisors = sorted(divisors)
-
-    required = defaultdict(list)
-    for 
+T = int(input())
+for _ in range(T):
+    S = input().strip()
+    print(count_odd_pairs(S))
