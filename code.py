2920f6a62bc4aa0b8298e9450b5e740af4ffd552
Analyzer <analyzer@test.com>
2026-02-09 10:22:19 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,12 +1,25 @@
-import math
+import heapq
 
-b, m = map(int, input().split())
-e = b + m - 1
-
-k = math.ceil(math.sqrt(b))
-l = math.floor(math.sqrt(e))
-
-if l < k:
-    print(0)
-else:
-    print(l - k + 1)
+def main():
+    n = int(input())
+    s = input().strip()
+    e = list(map(int, input().split()))
+    
+    next = [-1] * n
+    prev = [-1] * n
+    for i in range(n):
+        if i > 0:
+            prev[i] = i - 1
+        if i < n - 1:
+            next[i] = i + 1
+    
+    heap = []
+    for i in range(n - 1):
+        j = i + 1
+        if s[i] != s[j]:
+            diff = abs(e[i] - e[j])
+            heapq.heappush(heap, (diff, i, j))
+    
+    output = []
+    while heap:
+        diff, i,
