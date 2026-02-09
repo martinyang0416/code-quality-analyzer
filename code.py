022a73eab7faf4af09a06fefea4bffc464bd588b
Analyzer <analyzer@test.com>
2026-02-08 23:07:39 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,24 +1,19 @@
 import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    t = int(data[idx])
-    idx += 1
-    
-    for _ in range(t):
-        n = int(data[idx])
-        m = int(data[idx + 1])
-        idx += 2
-        grid = []
-        for i in range(n):
-            grid.append(data[idx].strip())
-            idx += 1
-        
-        # Precompute right[i][j]
-        right = [[0] * m for _ in range(n)]
-        for i in range(n):
-            j = 0
-            while j < m:
-      
+import builtins
+import re
