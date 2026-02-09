6bca6514722307c6d9e7e8456c4a52144598f86d
Analyzer <analyzer@test.com>
2026-02-09 10:04:22 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,20 +1,8 @@
-n, C, D = map(int, input().split())
-points = [int(input()) for _ in range(n)]
-points.sort()
+n = int(input())
+recipients = list(map(int, input().split()))
+counts = [0] * (n + 1)
 
-if n == 0:
-    print(0.0)
-else:
-    dp = [float('inf')] * n
-    dp[0] = C  # Covering just the first point with r=0
-    
-    for i in range(1, n):
-        for j in range(i + 1):
-            left = points[j]
-            right = points[i]
-            r = (right - left) / 2
-            cost = C + D * r
-            if j == 0:
-                prev_cost = 0
-            else:
-                prev_cost = dp[j - 
+for r in recipients:
+    counts[r] += 1
+
+print(' '.join(map(str, counts[1:n+1])))
