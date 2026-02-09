3dbe0c0a21438c665fc26787ff48f710f1c29c3a
Analyzer <analyzer@test.com>
2026-02-09 11:01:49 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-n = int(input())
-s = input().strip()
-
-herbs = [i for i, c in enumerate(s) if c == 'H']
-if len(herbs) < 5:
-    print("impossible")
-else:
-    herb_set = set(herbs)
-    found = False
-    for i in range(len(herbs)):
-        for j in range(i + 1, len(herbs)):
-            a0 = herbs[i]
-            a1 = herbs[j]
-            d = a1 - a0
-            a2 = a0 + 2 * d
-            a3 = a0 + 3 * d
-            a4 = a0 + 4 * d
-            if a2 in herb_set and a3 in herb_set and a4 in herb_set:
-                
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    b = list(map(int, input().split()))
+    b.sort(reverse=True)
+    mia = 0
+    friend = 0
+    for i in range(m):
+        if i % 2 == 0:
+            mia += b[i]
+        else:
+            friend += b[i]
+    if mia > friend:
+        print("WIN")
+    elif mia < friend:
+        print("LOSE")
+    else:
+        print("DRAW")
