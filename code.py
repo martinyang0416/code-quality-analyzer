23443a44034dc3e9b123a00310ba9f011183eab1
Analyzer <analyzer@test.com>
2026-02-09 10:29:39 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,27 +1,19 @@
-import math
+import sys
 
-def compute_gcd(arr):
-    current_gcd = arr[0]
-    for num in arr[1:]:
-        current_gcd = math.gcd(current_gcd, num)
-        if current_gcd == 1:
-            break
-    return current_gcd
+def can_partition(arr, target):
+    dp = [False] * (target + 1)
+    dp[0] = True
+    for num in arr:
+        for i in range(target, num - 1, -1):
+            if dp[i - num]:
+                dp[i] = True
+    return dp[target]
 
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    n = int(input[0])
-    a = list(map(int, input[1:n+1]))
+t = int(sys.stdin.readline())
+for _ in range(t):
+    h, v = map(int, sys.stdin.readline().split())
+    h_moves = list(map(int, sys.stdin.readline().split()))
+    v_moves = list(map(int, sys.stdin.readline().split()))
     
-    if n == 0:
-        print(0)
-        return
-    
-    g = compute_gcd(a)
-    
-    b = [num // g for num in a]
-    
-    if len(b) < 2:
-        print(0)
-        return
+    sum_h = sum(h_moves)
+    sum_v = sum
