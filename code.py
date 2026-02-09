cc2e0dbe78b091c216b8c05367c7c8603f808628
Analyzer <analyzer@test.com>
2026-02-09 09:58:13 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,10 +1,22 @@
-n, m = map(int, input().split())
-t = [int(input()) for _ in range(n)]
+import sys
 
-for k in range(1, m + 1):
-    for i in range(n - 1):
-        if t[i] % k > t[i+1] % k:
-            t[i], t[i+1] = t[i+1], t[i]
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        n = int(input[ptr])
+        ptr += 1
+        p = int(input[ptr])
+        ptr += 1
+        vertices_count = 4 * n
+        edges = []
+        for _e in range(p):
+            u = int(input[ptr])
+            ptr += 1
+            v = int(input[ptr])
+            ptr += 1
+            edges.append((u, v, _e + 1))  # 1-based index for edges
 
-for number in t:
-    print(number)
+        # Initialize gre
