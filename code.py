6687be8d3f52e53833e24f6f04d3d99508dbf527
Analyzer <analyzer@test.com>
2026-02-09 09:49:04 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,21 +1,24 @@
-n = int(input())
-stores_masks = []
-for _ in range(n):
-    bits = list(map(int, input().split()))
-    mask = 0
-    for i in range(10):
-        if bits[i] == 1:
-            mask += (1 << i)
-    stores_masks.append(mask)
+import sys
+import math
 
-profits = []
-for _ in range(n):
-    profits.append(list(map(int, input().split())))
+def extended_gcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return (g, x - (b // a) * y, y)
 
-max_total = -float('inf')
-for j_mask in range(1, 1 << 10):
-    total = 0
-    for i in range(n):
-        store_mask = stores_masks[i]
-        overlap = bin(j_mask & store_mask).count('1')
-        t
+def crt(a1, m1, a2, m2):
+    g, x, y = extended_gcd(m1, m2)
+    if (a1 - a2) % g != 0:
+        return None
+    lcm = m1 // g * m2
+    x0 = (a1 + (x * (a2 - a1) // g) % (m2 // g) * m1) % lcm
+    return x0
+
+def compute_same_days(d, r1, r2, n, m):
+    same = 0
+    for x in range(d):
+        a = x % n
+        b = x % m
+   
