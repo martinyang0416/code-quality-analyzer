1e1322b6b6e777de0fd8184d11da3ee88b232dbf
Analyzer <analyzer@test.com>
2026-02-09 10:52:57 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,5 +1,20 @@
-m = int(input())
-h = list(map(int, input().split()))
-h.sort()
-result = [h[i-1] for i in range(1, len(h))]
-print(' '.join(map(str, result)))
+def find_dominant_tree(t, test_cases):
+    results = []
+    for case in test_cases:
+        n, h = case
+        if n == 1:
+            results.append(1)
+            continue
+        
+        left = [0] * n
+        for i in range(1, n):
+            if h[i-1] < h[i]:
+                left[i] = left[i-1] + 1
+            else:
+                left[i] = 0
+        
+        right = [0] * n
+        for i in range(n-2, -1, -1):
+            if h[i] < h[i+1]:
+                right[i] = right[i+1] + 1
+      
