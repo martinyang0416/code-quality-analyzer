f96c5c710e1cb86e9a897ce6de724bb61d243a42
Analyzer <analyzer@test.com>
2026-02-09 09:50:27 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-def count_trimensional_coordinates(n, a, b, c):
-    l1 = a + 1
-    r1 = a + n
-    l2 = b + 1
-    r2 = b + n
-    l3 = c + 1
-    r3 = c + n
+n = int(input())
+h = list(map(int, input().split()))
+if n == 0:
+    print(0)
+    exit()
 
-    lower = max(l1, l2, l3)
-    upper = min(r1, r2, r3)
+dp = [0] * n
+dp[0] = 1
 
-    if lower > upper:
-        return 0
+for i in range(1, n):
+    j = i
+    while j > 0 and h[j-1] == h[i]:
+        j -= 1
+    option1 = dp[i-1] + 1
+    if j == 0:
+        option2 = 1
     else:
-        return upper - lower + 1
+        option2 = dp[j-1] + 1 if j > 0 else 1
+    dp[i] = min(option1, option2)
 
-# Read input
-n, a, b, c = map(int, input().split())
-print(count_trimensional_coordinates(n, a, b, c))
+print(dp[-1])
