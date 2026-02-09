32a345aef3d562adec47c89f0fe148d8d098e6ad
Analyzer <analyzer@test.com>
2026-02-09 10:33:36 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,24 +1,6 @@
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    
-    n = int(data[0])
-    q = int(data[1])
-    a = list(map(int, data[2:n + 2]))
-    diff = [0] * (n + 2)
-    
-    index = n + 2
-    for i in range(q):
-        l = int(data[index])
-        r = int(data[index + 1])
-        index += 2
-        diff[l] += 1
-        if r + 1 <= n:
-            diff[r + 1] -= 1
-    
-    weights = []
-    current = 0
-    for i in range(1, n + 1):
-        current += diff[i]
-        weight
+n = int(input())
+scores = list(map(int, input().split()))
+min_score = min(scores)
+third_student_score = scores[2]
+modified_score = (min_score ^ third_student_score) + 5
+print(modified_score)
