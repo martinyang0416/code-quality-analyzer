b327f2df1e2e4cc4633b5c4770f3bd97d085c409
Analyzer <analyzer@test.com>
2026-02-09 10:48:19 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,14 +1,10 @@
-def find_largest_number(Y):
-    if Y % 2 != 0:
-        return -1
-    for s in range(Y, -1, -2):
-        o = Y - s
-        if o >= 0 and o % 4 == 0:
-            return '7' * s + '1' * o
-    return -1
+n = int(input())
+a = list(map(int, input().split()))
 
-T = int(input())
-for _ in range(T):
-    Y = int(input())
-    result = find_largest_number(Y)
-    print(result if result != -1 else -1)
+if n < 3:
+    print(0.0)
+else:
+    a.sort()
+    trimmed = a[1:-1]
+    mean = sum(trimmed) / len(trimmed)
+    print("{0:.1f}".format(mean))
