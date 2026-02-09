35cfa71aad7291967a969d832ad826c1c71d3966
Analyzer <analyzer@test.com>
2026-02-09 10:43:05 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,14 +1,23 @@
-n = int(input())
-degrees = [0] * (n + 1)
-for _ in range(n - 1):
-    u, v = map(int, input().split())
-    degrees[u] += 1
-    degrees[v] += 1
+n, m = map(int, input().split())
+grid = [list(input().strip()) for _ in range(n)]
 
-s = 0
-for i in range(1, n + 1):
-    d = degrees[i]
-    s += d * (d - 1) // 2
+flowers = []
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == 'f':
+            flowers.append((i, j))
 
-total = (n - 1) + s
-print(total)
+if not flowers:
+    for row in grid:
+        print(''.join(row))
+    exit()
+
+min_r = min(i for i, j in flowers)
+max_r = max(i for i, j in flowers)
+min_c = min(j for i, j in flowers)
+max_c = max(j for i, j in flowers)
+
+single = (min_r == max_r) and (min_c == max_c)
+
+if single:
+    top = min_r - 1
