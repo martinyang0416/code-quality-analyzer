69a7f12e3698dff944759db0744571c73f0f2e88
Analyzer <analyzer@test.com>
2026-02-09 10:49:26 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,23 +1,23 @@
-import sys
+MOD = 10**9 + 7
 
-def can_reach_end(n, k, r):
-    # Check left path
-    current = 0
-    left_possible = True
-    for i in range(k-1, 0, -1):
-        current += r[i-1]
-        if current < 0:
-            left_possible = False
-            break
-    if left_possible:
-        return True
-    
-    # Check right path
-    current = 0
-    right_possible = True
-    for i in range(k+1, n+1):
-        if i > n:
-            break
-        current += r[i-1]
-        if current < 0:
-            right_possible = False
+n, k = map(int, input().split())
+m = n + k - 1
+
+fact = [1] * (m + 1)
+for i in range(1, m + 1):
+    fact[i] = fact[i-1] * i % MOD
+
+inv_fact = [1] * (m + 1)
+inv_fact[m] = pow(fact[m], MOD - 2, MOD)
+for i in range(m - 1, -1, -1):
+    inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD
+
+if k == 0:
+    print(1)
+else:
+    if m < k:
+        print(0)
+    else:
+        res = fact[m] * inv_fact[k] % MOD
+        res = res * inv_fact[m - k] % MOD
+        print(res)
