caa6560cdb20a8431f0ee7cdbf58ee90c8af816d
Analyzer <analyzer@test.com>
2026-02-09 09:52:09 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,12 +1,17 @@
-n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
-result = []
+from collections import Counter
 
-for s in range(2 * n - 1):
-    i_min = max(0, s - (n - 1))
-    i_max = min(s, n - 1)
-    for i in range(i_min, i_max + 1):
-        j = s - i
-        result.append(str(a[i][j]))
+def main():
+    arr = list(map(int, input().split()))
+    if not arr:
+        print()
+        return
+    freq = Counter(arr)
+    freq_counts = Counter(freq.values())
+    n = len(arr)
+    output = []
+    for i in range(n + 1):
+        output.append(str(freq_counts.get(i, 0)))
+    print(' '.join(output))
 
-print(' '.join(result))
+if __name__ == "__main__":
+    main()
