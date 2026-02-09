2d46da5200841784042fa1360764cbad9ce9528e
Analyzer <analyzer@test.com>
2026-02-09 10:12:10 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,21 +1,17 @@
-from collections import deque
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n+1)]
+count = 0
 
-def read_grid():
-    grid = []
-    for _ in range(3):
-        row = list(map(str, input().split()))
-        grid.extend(row)
-    return ''.join(grid)
+for i in range(1, n+1):
+    a = points[i-1]
+    b = points[i % (n+1)]
+    c = points[(i+1) % (n+1)]
+    abx = b[0] - a[0]
+    aby = b[1] - a[1]
+    bcx = c[0] - b[0]
+    bcy = c[1] - b[1]
+    dot = abx * bcx + aby * bcy
+    if dot > 0:
+        count += 1
 
-def main():
-    target = "123456780"
-    while True:
-        line = input()
-        if line == '-1':
-            break
-        # Read the next two lines
-        grid_part = [line.strip()]
-        grid_part.append(input().strip())
-        grid_part.append(input().strip())
-        # Process each test case
-        initi
+print(count)
