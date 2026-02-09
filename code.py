6fd9a24ebc2087a6eacac16798b695cff9e34f27
Analyzer <analyzer@test.com>
2026-02-09 10:05:48 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,14 +1,25 @@
-n, b = map(int, input().split())
-v = int(input())
-max_h = 0.0
+def is_all_even_digits(n):
+    s = str(n)
+    for c in s:
+        if c not in {'0', '2', '4', '6', '8'}:
+            return False
+    return True
 
-for _ in range(n):
-    x, h = map(int, input().split())
-    if v < x < b:
-        numerator = h * (b - v)
-        denominator = x - v
-        H = numerator / denominator
-        if H > max_h:
-            max_h = H
-
-print("{0:.6f}".format(max_h))
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0:
+        return False
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+    bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    for a in bases:
+        if a >= n:
+            continue
+        x = pow(a, d, n)
+  
