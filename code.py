f50635bfca219fb89154da790645420979489f18
Analyzer <analyzer@test.com>
2026-02-09 10:51:54 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,23 +1,13 @@
-import sys
-from collections import deque
+n = int(input())
+prefs = [list(map(int, input().split())) for _ in range(n)]
+gift = [0] * (n + 1)
+used = set()
 
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    t = int(input[ptr])
-    ptr += 1
-    for _ in range(t):
-        rows = int(input[ptr])
-        cols = int(input[ptr+1])
-        start_r = int(input[ptr+2])
-        start_c = int(input[ptr+3])
-        ptr +=4
-        
-        k = int(input[ptr])
-        ptr +=1
-        
-        closed = set()
-        for __ in range(k):
-            r = int(input[ptr])
-            c = int(input[ptr+1])
-      
+for i in range(1, n + 1):
+    for j in prefs[i - 1]:
+        if j != i and j not in used:
+            gift[i] = j
+            used.add(j)
+            break
+
+print(' '.join(map(str, gift[1:])))
