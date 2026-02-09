e658539e6622a438026019e123284962994d1186
Analyzer <analyzer@test.com>
2026-02-09 09:52:39 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,15 +1,6 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    sequence = [1]
-    current = 1
-    max_power = 0
-    while True:
-        next_val = current * 3
-        if next_val > n:
-            break
-        sequence.append(next_val)
-        current = next_val
-        max_power += 1
-    print(max_power)
-    print(' '.join(map(str, sequence)))
+    b = list(map(int, input().split()))
+    a = [1 if x == 0 else 0 for x in b]
+    print(' '.join(map(str, a)))
