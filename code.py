80410ab993eb84b0e157bb67554352de427657b7
Analyzer <analyzer@test.com>
2026-02-09 10:41:24 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,20 +1,22 @@
-n = int(input())
-employees = []
-sumT = 0
-for _ in range(n):
-    t, r = map(int, input().split())
-    employees.append((r, t))
-    sumT += t
+from collections import defaultdict
 
-employees.sort(reverse=True, key=lambda x: x[0])
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    N = int(data[0])
+    idx = 1
+    group_seats = defaultdict(list)
+    for _ in range(N):
+        s = int(data[idx])
+        g = int(data[idx+1])
+        group_seats[g].append(s)
+        idx += 2
 
-current_sum = 0
-max_val = 0
-for r, t in employees:
-    current_sum += t
-    temp = current_sum + r
-    if temp > max_val:
-        max_val = temp
+    # Each group must have at least one seat, so no need to handle empty groups
+    groups = list(group_seats.keys())
+    if len(groups) == 0:
+        print(0)
+        return
 
-answer = max(max_val, sumT)
-print(answer)
+    # P
