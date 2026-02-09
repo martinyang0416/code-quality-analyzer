4ea9a14b94e8311b36ebdaafd2dcc3e222d699e0
Analyzer <analyzer@test.com>
2026-02-09 10:12:29 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
-import sys
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n+1)]
+count = 0
 
-def main():
-    m = int(sys.stdin.readline())
-    b = list(map(int, sys.stdin.readline().split()))
-    p = int(sys.stdin.readline())
-    
-    bonuses = dict()  # key: (r, v), value: max c
-    
-    for _ in range(p):
-        rj, cj, vj = map(int, sys.stdin.readline().split())
-        rj -= 1  # zero-based index for resources
-        if vj == 0:
-            if (rj, cj) in bonuses:
-                del bonuses[(rj, cj)]
-        else:
-            key = (rj, vj)
-            if key in bonus
+for i in range(1, n+1):
+    a = points[i-1]
+    b = points[i % (n+1)]
+    c = points[(i+1) % (n+1)]
+    abx = b[0] - a[0]
+    aby = b[1] - a[1]
+    bcx = c[0] - b[0]
+    bcy = c[1] - b[1]
+    dot = abx * bcx + aby * bcy
+    if dot > 0:
+        count += 1
+
+print(count)
