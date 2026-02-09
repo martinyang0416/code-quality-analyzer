6c66e39b97070ab1b4ed23e8a1f7ec0a761ca31e
Analyzer <analyzer@test.com>
2026-02-09 10:05:31 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,14 @@
-def determine_holder():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx += 1
-    for _ in range(T):
-        N = int(input[idx])
-        ID = int(input[idx + 1])
-        idx += 2
-        current = ID
-        last_passer = None
-        for _ in range(N):
-            cmd = input[idx]
-            if cmd == 'P':
-                # Process P command
-                new_id = int(input[idx + 1])
-                idx += 2
-                last_passer = current
+n, b = map(int, input().split())
+v = int(input())
+max_h = 0.0
+
+for _ in range(n):
+    x, h = map(int, input().split())
+    if v < x < b:
+        numerator = h * (b - v)
+        denominator = x - v
+        H = numerator / denominator
+        if H > max_h:
+            max_h = H
+
+print("{0:.6f}".format(max_h))
