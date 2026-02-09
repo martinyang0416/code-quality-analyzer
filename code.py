f595cf7e3cf751dca5342e9c03d4792fa62b089e
Analyzer <analyzer@test.com>
2026-02-09 10:13:06 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,23 +1,24 @@
 def main():
     import sys
     input = sys.stdin.read().split()
-    ptr = 0
-    while True:
-        n = int(input[ptr])
-        m = int(input[ptr+1])
-        ptr += 2
-        if n == 0 and m == 0:
-            break
-        vectors = []
-        for _ in range(n):
-            s = input[ptr]
-            ptr += 1
-            num = int(s, 2)
-            vectors.append(num)
-        # Compute S
-        S = 0
-        for num in vectors:
-            S ^= num
-        if S == 0:
-            print(n)
-      
+    idx = 0
+    M = int(input[idx])
+    idx += 1
+    D = int(input[idx])
+    idx += 1
+    
+    stations = []
+    for _ in range(M):
+        f = int(input[idx])
+        p = int(input[idx + 1])
+        stations.append((p, f))
+        idx += 2
+    
+    stations.sort()  # Sort by position
+    left = 0
+    current_sum = 0
+    max_sum = 0
+    
+    for right in range(len(stations)):
+        current_sum += stations[right][1]
+        while s
