7451567f4358d70d1cdb7b77ffd3be43d5e45405
Analyzer <analyzer@test.com>
2026-02-09 10:47:12 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-n, k = map(int, input().split())
-d = list(map(int, input().split()))
-
-if d.count(0) != 1:
-    print(-1)
-    exit()
-
-max_d = max(d)
-
-layers = {}
-for i in range(n):
-    node = i + 1
-    dist = d[i]
-    if dist not in layers:
-        layers[dist] = []
-    layers[dist].append(node)
-
-# Check if all layers from 0 to max_d-1 are present when needed
-for current_d in range(max_d):
-    if current_d not in layers or len(layers[current_d]) == 0:
-        if (current_d + 1) in layers and len(layers[current_d 
+m, b = map(int, input().split())
+prices = list(map(int, input().split()))
+prices.sort()
+total = 0
+count = 0
+for price in prices:
+    if total + price <= b:
+        total += price
+        count += 1
+    else:
+        break
+print(count)
