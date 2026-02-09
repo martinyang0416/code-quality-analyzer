56bc08b64317165ab699bacadd332a0a4d46fbf4
Analyzer <analyzer@test.com>
2026-02-09 10:00:33 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,14 @@
-def main():
-    import sys
-    n = int(sys.stdin.readline())
-    blocks = []
-    for _ in range(n):
-        a, b, v = map(int, sys.stdin.readline().split())
-        blocks.append((a, b, v))
-    
-    dp = [0] * 5  # indexes 1-4 used
-    for a, b, v in blocks:
-        before_dp = dp.copy()
-        temp_dp = before_dp.copy()
-        for s, e in [(a, b), (b, a)]:
-            if before_dp[s] + v > temp_dp[e]:
-                temp_dp[e] = before_dp[s] + v
-        dp = temp_dp.copy()
-    
-    print(max
+n = int(input())
+islands = set()
+for _ in range(n):
+    x, y = map(int, input().split())
+    islands.add((x, y))
+
+total = 0
+
+for x, y in islands:
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        if (x + dx, y + dy) not in islands:
+            total += 1
+
+print(total)
