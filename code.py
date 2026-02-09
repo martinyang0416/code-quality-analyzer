8d7d07121803d398aef94f28bc68445f9a157952
Analyzer <analyzer@test.com>
2026-02-09 10:18:17 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,11 @@
-import sys
+b = int(input())
+m = input().strip()
 
-def longest_common_substring(s1, s2):
-    s1 = s1.lower()
-    s2 = s2.lower()
-    n = len(s1)
-    m = len(s2)
-    max_length = 0
-    prev_row = [0] * (m + 1)
-    for i in range(n):
-        current_row = [0] * (m + 1)
-        for j in range(m):
-            if s1[i] == s2[j]:
-                current_row[j+1] = prev_row[j] + 1
-                if current_row[j+1] > max_length:
-                    max_length = current_row[j+1]
-            else:
-                current_row[j+1] = 0
-       
+if m == '0':
+    print(0)
+else:
+    res = 0
+    for c in m:
+        d = int(c)
+        res = res * b + d
+    print(res)
