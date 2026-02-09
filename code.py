549742c4b4f0919cd563b64990780c936c947598
Analyzer <analyzer@test.com>
2026-02-09 09:47:04 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,24 +1,22 @@
 def main():
     import sys
     input = sys.stdin.read().split()
-    m = int(input[0])
-    elements = list(map(int, input[1:]))
-    elements.sort()
-
-    # Check frequency condition
-    freq = {}
-    for num in elements:
-        if num in freq:
-            freq[num] += 1
-        else:
-            freq[num] = 1
-
-    is_even = (m % 2 == 0)
-    valid = True
-
-    if is_even:
-        for count in freq.values():
-            if count % 2 != 0:
-                valid = False
-                break
-    else:
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        N = int(input[ptr])
+        ptr += 1
+        c = list(map(int, input[ptr:ptr+N]))
+        ptr += N
+        S = sum(c)
+        if S % N != 0:
+            print(-1)
+            continue
+        avg = S // N
+        running = 0
+        transfers = 0
+        for i in range(N):
+            running += c[i] - avg
+            if i != N - 1:
+                trans
