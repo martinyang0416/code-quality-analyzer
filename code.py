10f4b19fbd674b7e5b5442af451aff95b55aa7c9
Analyzer <analyzer@test.com>
2026-02-09 10:23:12 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,23 +1,2 @@
-from collections import Counter
-
-def main():
-    s = input().strip()
-    k = int(input())
-    
-    if not s:
-        print(0)
-        print()
-        return
-    
-    freq = Counter(s)
-    distinct = len(freq)
-    
-    if distinct <= k:
-        print(len(s))
-        print(s)
-        return
-    else:
-        # Sort by frequency descending, then take top k
-        letters = sorted(freq.items(), key=lambda x: (-x[1], x[0]))[:k]
-        selected_chars = {char for char, count in letters}
-        total
+n = int(input())
+print(5 + 3 * n)
