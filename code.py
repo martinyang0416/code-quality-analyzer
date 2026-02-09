df9dd2b2324bf2f7f60072500bb7bfbb2425cb2c
Analyzer <analyzer@test.com>
2026-02-09 10:07:57 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
-MOD = 10**9 + 7
-
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    n = int(data[0])
-    k = int(data[1])
-    
-    # Precompute factorials and inverse factorials up to 3999
-    max_fact = 3999
-    fact = [1] * (max_fact + 1)
-    for i in range(1, max_fact + 1):
-        fact[i] = fact[i-1] * i % MOD
-    
-    inv_fact = [1] * (max_fact + 1)
-    inv_fact[max_fact] = pow(fact[max_fact], MOD - 2, MOD)
-    for i in range(max_fact - 1, -1, -1):
-        inv_fact[i] = in
+def process(arr):
+    while True:
+        if not arr:
+            break
+        runs = []
+        current = arr[0]
+        count = 1
+        for num in arr[1:]:
+            if num == current:
+                count += 1
+            else:
+                runs.append((current, count))
+                current = num
+                count = 1
+        runs.append((current, count))
+        to_remove = [i for i, (c, cnt) in enumerate(runs) if cnt >= 3]
+        if not to_remove:
+            break
+        
