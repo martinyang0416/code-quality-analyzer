4c3ebba39a3a877faefb794cc5126c702b9d6ae2
Analyzer <analyzer@test.com>
2026-02-09 10:26:12 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,8 @@
-import math
-import itertools
-
 n = int(input())
-radii = [int(input()) for _ in range(n)]
-
-max_circumference = 0.0
-
-for k in range(3, n + 1):
-    for subset in itertools.combinations(radii, k):
-        sum_r = sum(subset)
-        for arrangement in itertools.permutations(subset):
-            valid = True
-            for i in range(k):
-                a = arrangement[i]
-                b = arrangement[(i + 1) % k]
-                if a + b >= sum_r:
-                    valid = False
-                
+total = 0.0
+for _ in range(n):
+    score = float(input())
+    total += score
+average = total / n
+adjusted_average = average + 10
+print("{0:.2f}".format(round(adjusted_average, 2)))
