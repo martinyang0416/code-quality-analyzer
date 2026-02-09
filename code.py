09e54eb82958533fcb586cba5abafcaf7324eeb1
Analyzer <analyzer@test.com>
2026-02-09 10:44:54 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,2 +1,11 @@
-M = int(input())
-print('1 ' * M)
+n = int(input())
+a = list(map(int, input().split()))
+min_candidate = None
+
+for x in range(1, 1001):
+    temp = a + [x]
+    temp_sorted = sorted(temp)
+    if min_candidate is None or temp_sorted < min_candidate:
+        min_candidate = temp_sorted.copy()
+
+print(' '.join(map(str, min_candidate)))
