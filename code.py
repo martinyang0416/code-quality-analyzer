211771d6a0091c963b4f0b5c5180b69e2d9dad8a
Analyzer <analyzer@test.com>
2026-02-09 10:37:11 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,27 +1,22 @@
-n_players = int(input())
-A = 0
-P = 0
+n, r, s = map(int, input().split())
+books = list(map(int, input().split()))
+books.sort()
 
-for _ in range(n_players):
-    name, cat = input().split()
-    if cat == 'amateur':
-        A += 1
+count = 0
+
+for b in books:
+    if r < 0 or s < 0:
+        continue
+    initial_a = min(r, b // 3)
+    rem = b - initial_a * 3
+    if rem <= s:
+        count += 1
+        r -= initial_a
+        s -= rem
     else:
-        P += 1
-
-max_ap = max(A, P)
-min_ap = min(A, P)
-
-n = 1
-while True:
-    if n % 2 == 0:
-        half = (n * n) // 2
-        if max_ap <= half:
-            print(n)
-            break
-    else:
-        large_half = (n * n + 1) // 2
-        small_half = (n * n - 1) // 2
-        if max_ap <= large_half and min_ap <= small_half:
-            print(n)
-  
+        numerator = b - s + 2
+        required_a_min = numerator // 3
+        required_a_min = max(required_a_min, 0)
+        available_a = min(r, b // 3)
+        if available_a >= required_a_min:
+ 
