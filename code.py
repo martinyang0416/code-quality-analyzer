147b4d766e5773879126e914774c7f8328dadb0c
Analyzer <analyzer@test.com>
2026-02-09 09:47:45 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
-import heapq
+class DSU:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
 
-def dijkstra(graph, start, num_nodes):
-    dist = [float('inf')] * (num_nodes + 1)
-    dist[start] = 0
-    heap = []
-    heapq.heappush(heap, (0, start))
-    visited = [False] * (num_nodes + 1)
-    
-    while heap:
-        current_dist, u = heapq.heappop(heap)
-        if visited[u]:
-            continue
-        visited[u] = True
-        for (v, w) in graph[u]:
-            if dist[v] > current_dist + w:
-                dist[v] = current_dist + w
-                heapq.heappush(heap, 
+    def find(self, x):
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]  # Path compression
+            x = self.parent[x]
+        return x
+
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return False
+        if self.rank[x_root] < self.rank[y_root]:
+            
