097a3361e8d5a3dba4f299627c0dbbbdf5e01ec7
Analyzer <analyzer@test.com>
2026-02-09 09:50:13 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,10 +1,21 @@
-t = int(input())
-for _ in range(t):
-    n, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    dp = [0] * (k + 1)
-    dp[0] = 1
-    for num in a:
-        for s in range(k, num - 1, -1):
-            dp[s] += dp[s - num]
-    print(dp[k])
+n = int(input())
+h = list(map(int, input().split()))
+if n == 0:
+    print(0)
+    exit()
+
+dp = [0] * n
+dp[0] = 1
+
+for i in range(1, n):
+    j = i
+    while j > 0 and h[j-1] == h[i]:
+        j -= 1
+    option1 = dp[i-1] + 1
+    if j == 0:
+        option2 = 1
+    else:
+        option2 = dp[j-1] + 1 if j > 0 else 1
+    dp[i] = min(option1, option2)
+
+print(dp[-1])
