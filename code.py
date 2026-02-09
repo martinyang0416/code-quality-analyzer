3f9e9d50d0a388cb12bf0a9e2a02bffb65d76fdc
Analyzer <analyzer@test.com>
2026-02-09 10:01:29 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,21 @@
-MOD = 998244353
-
 def main():
     import sys
-    n, k = map(int, sys.stdin.readline().split())
-    a = list(map(int, sys.stdin.readline().split()))
-    a.sort()
+    input = sys.stdin.read().split()
+    n = int(input[0])
+    numbers = list(map(int, input[1:n+1]))
     
-    # Precompute factorial and inverse factorial up to n
-    max_n = n
-    fact = [1] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        fact[i] = fact[i-1] * i % MOD
+    counts = [0, 0, 0]
+    for num in numbers:
+        mod = num % 3
+        counts[mod] += 1
     
-    inv_fact = [1] * (max_n + 1)
-    inv_fact[max_n] = pow(fact[max_n], MOD-2, MOD)
-    for i in range(max_n - 1, -1, -1):
-        inv_fact[i] = inv_fact[i+1]
+    c0, c1, c2 = counts
+    total = 0
+    
+    # Triplets from each individual group
+    if c0 >= 3:
+        total += c0 * (c0 - 1) * (c0 - 2) // 6
+    if c1 >= 3:
+        total += c1 * (c1 - 1) * (c1 - 2) // 6
+    if c2 >= 3:
+        total += c2 * (c2 - 1) * (c2 - 2
