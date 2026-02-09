62c8c6b1a237dda95fa12c6513afcff9a2606188
Analyzer <analyzer@test.com>
2026-02-08 22:53:02 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
-A_str = "What are you doing while sending "  # length 32
-B_str = " Are you busy? Will you send "      # length 29
-f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"
+fixed_part1 = "What are you doing while sending "  # 33 characters
+fixed_part2 = " Are you busy? Will you send "     # 29 characters
+f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"  # length 75
 
 def solve():
     import sys
     input = sys.stdin.read().split()
     q = int(input[0])
     idx = 1
-    output = []
+    res = []
     for _ in range(q):
         n = int(input[idx])
         k = int(input[idx+1])
         idx +=2
-
-        if n > 60:
-            # Proceed since L_n is way larger than k
-            c
+        
+        def get_char(n, k):
+            if n ==0
