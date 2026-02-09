48dedd80bb92a1ed87d7f2be07086db6f5bbc137
Analyzer <analyzer@test.com>
2026-02-09 10:03:34 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,17 +1,18 @@
-n, k = map(int, input().split())
-s = list(map(int, input().split()))
+n_target, h_target = map(int, input().split())
 
-prefix = [0] * (n + 1)
-for i in range(n):
-    prefix[i + 1] = prefix[i] + s[i]
+max_n = n_target
+max_h = 2 * max_n  # Theoretical maximum height for n nodes is n.
 
-max_avg = 0.0
+# Initialize DP and cumulative sum arrays
+dp = [[0] * (max_h + 2) for _ in range(max_n + 2)]
+sum_n_h = [[0] * (max_h + 2) for _ in range(max_n + 2)]
 
-for L in range(k, n + 1):
-    for i in range(n - L + 1):
-        total = prefix[i + L] - prefix[i]
-        avg = total / L
-        if avg > max_avg:
-            max_avg = avg
+# Base case: empty tree has height 0
+for h in range(max_h + 1):
+    sum_n_h[0][h] = 1
 
-print("{0:.15f}".format(max_avg))
+for n in range(1, max_n + 1):
+    for h in range(1, max_h + 1):
+        total = 0
+        for i in range(n):
+            j = n - 1 
