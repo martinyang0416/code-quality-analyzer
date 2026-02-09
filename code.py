815db9fa27de39d77468555825707135cc329d7e
Analyzer <analyzer@test.com>
2026-02-09 10:01:44 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,18 +1,14 @@
-MOD = 998244353
+n, d = map(int, input().split())
+a = list(map(int, input().split()))
 
-def main():
-    import sys
-    n, k = map(int, sys.stdin.readline().split())
-    a = list(map(int, sys.stdin.readline().split()))
-    a.sort()
-    
-    # Precompute factorial and inverse factorial up to n
-    max_n = n
-    fact = [1] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        fact[i] = fact[i-1] * i % MOD
-    
-    inv_fact = [1] * (max_n + 1)
-    inv_fact[max_n] = pow(fact[max_n], MOD-2, MOD)
-    for i in range(max_n - 1, -1, -1):
-        inv_fact[i] = inv_fact[i+1]
+mod_counts = {0: 1}
+current_sum = 0
+result = 0
+
+for num in a:
+    current_sum += num
+    mod = current_sum % d
+    result += mod_counts.get(mod, 0)
+    mod_counts[mod] = mod_counts.get(mod, 0) + 1
+
+print(result)
