953bfb4d164a86f4fae2598aa4c2a3267bedb59a
Analyzer <analyzer@test.com>
2026-02-09 09:55:31 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,22 +1,17 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx +=1
-    for _ in range(T):
-        N = int(input[idx])
-        idx +=1
-        W = list(map(int, input[idx:idx+N]))
-        idx +=N
-        Y = int(input[idx])
-        idx +=1
-        
-        fountains = []
-        for i in range(N):
-            t1 = i / Y
-            t2 = (N - 1 - i)
-            earliest = min(t1, t2)
-            fountains.append((earliest, i, t1, t2))
-        
-        #
+W, H = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(H)]
+
+perimeter = 0
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+for i in range(H):
+    for j in range(W):
+        if grid[i][j] == 1:
+            for d in directions:
+                ni = i + d[0]
+                nj = j + d[1]
+                if ni < 0 or ni >= H or nj < 0 or nj >= W:
+                    perimeter += 1
+                else:
+                    if grid[ni][nj] == 0:
+                       
