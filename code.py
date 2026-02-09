bd893cb15bd957482ec221748535dd7d69f77515
Analyzer <analyzer@test.com>
2026-02-09 10:36:05 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,11 +1,23 @@
-n = int(input())
-A = list(map(int, input().split()))
-m = int(input())
-B = list(map(int, input().split()))
-
-combined = A + B
-unique_set = set(combined)
-sorted_union = sorted(unique_set)
-
-for num in sorted_union:
-    print(num)
+def minimal_cost():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    t = int(data[0])
+    idx = 1
+    for _ in range(t):
+        n = int(data[idx])
+        idx += 1
+        c = list(map(int, data[idx:idx+n]))
+        idx += n
+        
+        prefix = [0] * (n + 1)
+        for i in range(1, n+1):
+            prefix[i] = prefix[i-1] + c[i-1]
+        
+        if n == 0:
+            print(0)
+            continue
+        
+        right_dp = [0] * (n + 1)
+        min_righ
