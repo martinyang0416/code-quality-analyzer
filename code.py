e04779bdda9125052273657c4d95c5e301ad0017
Analyzer <analyzer@test.com>
2026-02-09 10:15:17 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,19 +1,3 @@
-def find_min_k(s):
-    if s == 0:
-        return 0
-    current_fact = 1
-    current_sum = 1
-    k = 1
-    if current_sum >= s:
-        return k
-    while True:
-        k += 1
-        current_fact *= k
-        current_sum = sum(int(d) for d in str(current_fact))
-        if current_sum >= s:
-            return k
-
-T = int(input())
-for _ in range(T):
-    s = int(input())
-    print(find_min_k(s))
+n = int(input())
+student_ids = list(map(int, input().split()))
+print(len(set(student_ids)))
