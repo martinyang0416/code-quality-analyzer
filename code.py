fcaf284d8e78f02dc62e55bcf0ee592acb07c2f6
Analyzer <analyzer@test.com>
2026-02-09 10:46:56 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
-a, b = map(int, input().split())
-
-def minimal_capacitors(a, b):
-    sum_q = 0
-    while b != 0:
-        sum_q += a // b
-        a, b = b, a % b
-    return sum_q
-
-print(minimal_capacitors(a, b))
+m, b = map(int, input().split())
+prices = list(map(int, input().split()))
+prices.sort()
+total = 0
+count = 0
+for price in prices:
+    if total + price <= b:
+        total += price
+        count += 1
+    else:
+        break
+print(count)
