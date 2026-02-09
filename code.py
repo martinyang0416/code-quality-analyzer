b1a7fde43cf4db3e1d160cbb245fcbabb70abc66
Analyzer <analyzer@test.com>
2026-02-09 10:26:59 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,21 +1,16 @@
-import sys
-from collections import deque
+n = int(input())
+a = list(map(int, input().split()))
+from collections import defaultdict
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    ptr = 0
-    t = int(data[ptr])
-    ptr += 1
-    for _ in range(t):
-        n = int(data[ptr])
-        k = int(data[ptr+1])
-        ptr += 2
-        friends = list(map(int, data[ptr:ptr+k]))
-        ptr += k
-        adj = [[] for _ in range(n + 1)]
-        for __ in range(n - 1):
-            u = int(data[ptr])
-            v = int(data[ptr+1])
-            adj[u].append(v)
-            adj
+freq = defaultdict(int)
+
+for num in a:
+    msb = num.bit_length() - 1
+    freq[msb] += 1
+
+total = 0
+for count in freq.values():
+    if count >= 2:
+        total += count * (count - 1) // 2
+
+print(total)
