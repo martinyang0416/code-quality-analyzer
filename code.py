744a18a700be555354bdf58e12f1e9257fc7f9b7
Analyzer <analyzer@test.com>
2026-02-09 09:53:13 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,25 +1,9 @@
-import math
-
-def get_divisors(p):
-    divisors = set()
-    for i in range(1, int(math.isqrt(p)) + 1):
-        if p % i == 0:
-            divisors.add(i)
-            divisors.add(p // i)
-    return sorted(divisors)
-
-def count_pairs(p, q):
-    if p < q:
-        return 0
-    divisors = get_divisors(p)
-    count = 0
-    for d in divisors:
-        if d < q:
-            continue
-        if p % d != 0:
-            continue
-        v = p // d
-        if v < q:
-            continue
-        x = d - q
-    
+m = int(input())
+A = list(map(int, input().split()))
+n = int(input())
+B = list(map(int, input().split()))
+C = A + B
+max_val = max(C)
+min_val = min(C)
+result = max_val - min_val
+print("{0:.9f}".format(result))
