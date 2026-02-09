26e512002d54f698700dc42586dcc5d37b8917c0
Analyzer <analyzer@test.com>
2026-02-09 10:34:24 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,21 +1,14 @@
-MOD = 10**9 + 7
-
 n = int(input())
-c = list(map(int, input().split()))
-d = list(map(int, input().split()))
-q = int(input())
-y_list = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+arr.sort()
+current_max = 0
 
-max_a1 = c[0] if n > 0 else 0
-counts = [0] * (max_a1 + 1)
+for num in arr:
+    if num == 0:
+        continue
+    if num > current_max + 1:
+        print(current_max + 1)
+        exit()
+    current_max += num
 
-def compute_count(a1):
-    if a1 < 0 or a1 > c[0]:
-        return 0
-    current = [0] * (c[0] + 1)
-    current[a1] = 1
-    for i in range(n-1):
-        next_dp = [0] * (c[i+1] + 1)
-        d_val = d[i]
-        for a_prev in range(len(current)):
-            if current[a_prev] 
+print(current_max + 1)
