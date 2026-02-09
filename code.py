dd281f7c2631c0e5281347efeb22203b54a0b01f
Analyzer <analyzer@test.com>
2026-02-09 10:10:21 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,13 +1,3 @@
-n, x = map(int, input().split())
-k = int(input())
-c = 0
-for _ in range(k):
-    t, _ = map(int, input().split())
-    if t == 1:
-        c += 1
-
-u = (x - 1) - k
-min_skipped = 0
-max_skipped = u
-
-print(min_skipped, max_skipped)
+n, a, b = map(int, input().split())
+new_index = (a - 1 + b) % n
+print(new_index + 1)
