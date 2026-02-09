2c4a9e685027207f7041a4bb501c3c36c11d53dd
Analyzer <analyzer@test.com>
2026-02-09 10:04:07 -0800

    Update code for analysis

--- code.py
+++ code.py
@@ -1,15 +1,20 @@
-M = int(input())
-lines = []
-for _ in range(M):
-    d, c = map(int, input().split())
-    lines.append((d, c))
+n, C, D = map(int, input().split())
+points = [int(input()) for _ in range(n)]
+points.sort()
 
-number_str = ''.join(str(d) * c for d, c in lines)
-rounds = 0
-
-while len(number_str) > 1:
-    sum_digits = sum(int(ch) for ch in number_str)
-    rounds += 1
-    number_str = str(sum_digits)
-
-print(rounds)
+if n == 0:
+    print(0.0)
+else:
+    dp = [float('inf')] * n
+    dp[0] = C  # Covering just the first point with r=0
+    
+    for i in range(1, n):
+        for j in range(i + 1):
+            left = points[j]
+            right = points[i]
+            r = (right - left) / 2
+            cost = C + D * r
+            if j == 0:
+                prev_cost = 0
+            else:
+                prev_cost = dp[j - 
